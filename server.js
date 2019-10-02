09060e8ea840da8c8ab7fef28c2d41050633fb84
milano12345 <dmilano857@gmail.com>
2019-10-02 15:45:57 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
-const express = 'express';
+const express = "express";
+const helmet = require("helmet");
+const logger = require("morgan");
+const userRouter = require("./users/userRouter");
+const postRouter = require("./posts/postRouter");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use("/api/posts", postRouter);
+server.use("/api/users", userRouter);
 //custom middleware
 
-function logger(req, res, next) {
+server.use(express.json());
+server.use(helmet());
+server.use(logger("dev"));
 
-};
+function logger(req, res, next) {
+  console.log(`${req.method} Request`);
+  next();
+}
 
 module.exports = server;
